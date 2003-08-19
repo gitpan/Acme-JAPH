981ff4bb48dd72e9e946b13b666cff4aff8668c6
Casey West <casey@geeknest.com>
2003-08-19 17:47:34 +0000

    Import of CWEST/Acme-JAPH-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-JAPH
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         CWEST/Acme-JAPH-0.01.tar.gz
    gitpan-cpan-author:       CWEST
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/JAPH.pm
@@ -0,0 +1,31 @@
+package Acme::JAPH;
+$Acme::JAPH::VERSION = '0.01';
+print "Just another Perl Hacker,\n";
+exit;
+
+=pod
+
+=head1 NAME
+
+Acme::JAPH - Turn anything into a JAPH
+
+=head1 SYNOPSIS
+
+    use Acme::JAPH
+    a;ltiucqpoc3iqp04tu1 -394t8[ieufea[=09E U 0ET 9u [4tu]]]
+
+=head1 DESCRIPTION
+
+C<Acme::JAPH> turns anything into a JAPH.  Very useful, and simple.
+
+=head1 AUTHOR
+
+Casey West <F<casey@geeknest.com>>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2003 Casey West, All Rights Reserved.
+This module is released under the same terms as Perl itself.
+
+=cut
+
